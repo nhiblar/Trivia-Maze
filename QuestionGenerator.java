4e6d91d7bceffe94dde1b086106287bbd31c0a4f
TillerQ <61754456+TillerQ@users.noreply.github.com>
2020-05-25 22:52:43 -0700

    Add files via upload

--- QuestionGenerator.java
+++ QuestionGenerator.java
@@ -1,5 +1,41 @@
 package TrivaMaze;
 
+import java.util.Random;
+
 public class QuestionGenerator {
+	private Random question;
+	
+	public QuestionGenerator() {
+		question = new Random();
+	}
+	
+	public QuestionHandler questionCreation(String obj) {
+		QuestionHandler ques;
+		
+		switch(obj.toLowerCase()) {
+		case "muiltpleChoice":
+			break;
+		case "trueFalse":
+			break;
+		case "short":
+			break;
+		default:	
+		}
+		return ques;
+	}
+	
+	
+	public Question getQuestion() {
+		switch (question.nextInt(3)) {
+			case 0:
+				return questionCreation("truefalse");
+			case 1:
+				return questionCreation("short");
+			case 2:
+				return questionCreation("muiltipleChoice");
+			default:
+				return questionCreation("truefalse");
+		}
+	}
 	
 }
