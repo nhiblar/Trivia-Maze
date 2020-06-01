515748a047f8af151fb177f270e5533970d0e292
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-06-01 16:36:21 -0700

    Add files via upload

--- TriviaController.java
+++ TriviaController.java
@@ -19,6 +19,8 @@ public class TriviaController {
 		this.theView = theView;
 		this.theModel = theModel;
 		this.theView.addSubmitListener(new SubmitListener());
+		this.theView.addMenuListener(new MenuListener());
+		this.theView.addDoorListener(new DoorListener());
 	}
 
 	class SubmitListener implements ActionListener{
@@ -27,54 +29,96 @@ public class TriviaController {
 
 			if(e.getSource() == theView.getSubmitButton()) {
 
-				System.out.println("Clicked submit button.");
-				int firstNumber = 0;
+				for (Enumeration<AbstractButton> buttons = theView.getMultButtons().getElements(); buttons.hasMoreElements();) {
+					AbstractButton button = buttons.nextElement();
 
-				try {
-					firstNumber = theView.getaNumber();
-					theModel.setSubmitValue(firstNumber);
-					theView.setSubmitValue(theModel.getSubmitValue());
-
-				}
-				catch(NumberFormatException ex) {
-
-					theView.displayErrorMessage("You need to enter an Integer");
-				}
-				
-				if(theView.getAnswer1().isSelected()) {
-					System.out.println("Answer 1 selected");
-				}
-				if(theView.getAnswer2().isSelected()) {
-					System.out.println("Answer 2 selected");
-				}
-				if(theView.getAnswer3().isSelected()) {
-					System.out.println("Answer 3 selected.");
-				}
-				if(theView.getAnswer4().isSelected()) {
-					System.out.println("Answer 4 selected.");
-				}
-				
+					if (button.isSelected()) {
+						multAnswer = button.getText();
+						System.out.println(multAnswer + " from Submit Button");
+					}
+				}//end for check multButton source
 			}//end if e.getSource
-
-		}	
+		}//end actionPerformed method
 	}//end class SubmitListener
+	
+	
+	class MenuListener implements ActionListener{
 
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == theView.getSave()){
+				System.out.println("Saved in Menu, File clicked");
 
-	class RadioListener implements ActionListener{
+			}if(e.getSource() == theView.getLoad()){
+				System.out.println("Load in Menu, File clicked");
+
+			}if(e.getSource() == theView.getQuit()){
+				System.out.println("Quit in Menu, File clicked");
+
+			}if(e.getSource() == theView.getAbout()){
+				theView.aboutPanel();
+			}
+		}
+	}
+	
+	class DoorListener implements ActionListener{
 
 		public void actionPerformed(ActionEvent e) {
 
-			for (Enumeration<AbstractButton> buttons = theView.getMultButtons().getElements(); buttons.hasMoreElements();) {
-				AbstractButton button = buttons.nextElement();
-
-				if (button.isSelected()) {
-					multAnswer = button.getText();
-					System.out.println(multAnswer);
-				}
-
+			if(e.getSource() == theView.getBut1()){
+				System.out.println("Door 1 clicked");
+			}if(e.getSource() == theView.getBut2()){
+				System.out.println("Door 2 clicked");
+			}if(e.getSource() == theView.getBut3()){
+				System.out.println("Door 3 clicked");
+			}if(e.getSource() == theView.getBut4()){
+				System.out.println("Door 4 clicked");
+			}if(e.getSource() == theView.getBut5()){
+				System.out.println("Door 5 clicked");
+			}if(e.getSource() == theView.getBut6()){
+				System.out.println("Door 6 clicked");
+			}if(e.getSource() == theView.getBut7()){
+				System.out.println("Door 7 clicked");
+			}if(e.getSource() == theView.getBut8()){
+				System.out.println("Door 8 clicked");
+			}if(e.getSource() == theView.getBut9()){
+				System.out.println("Door 9 clicked");
+			}if(e.getSource() == theView.getBut10()){
+				System.out.println("Door 10 clicked");
+			}if(e.getSource() == theView.getBut11()){
+				System.out.println("Door 11 clicked");
+			}if(e.getSource() == theView.getBut12()){
+				System.out.println("Door 12 clicked");
+			}if(e.getSource() == theView.getBut13()){
+				System.out.println("Door 13 clicked");
+			}if(e.getSource() == theView.getBut14()){
+				System.out.println("Door 14 clicked");
+			}if(e.getSource() == theView.getBut15()){
+				System.out.println("Door 15 clicked");
+			}if(e.getSource() == theView.getBut16()){
+				System.out.println("Door 16 clicked");
+			}if(e.getSource() == theView.getBut17()){
+				System.out.println("Door 17 clicked");
+			}if(e.getSource() == theView.getBut18()){
+				System.out.println("Door 18 clicked");
+			}if(e.getSource() == theView.getBut19()){
+				System.out.println("Door 19 clicked");
+			}if(e.getSource() == theView.getBut20()){
+				System.out.println("Door 20 clicked");
+			}if(e.getSource() == theView.getBut21()){
+				System.out.println("Door 21 clicked");
+			}if(e.getSource() == theView.getBut22()){
+				System.out.println("Door 22 clicked");
+			}if(e.getSource() == theView.getBut23()){
+				System.out.println("Door 23 clicked");
+			}if(e.getSource() == theView.getBut24()){
+				System.out.println("Door 24 clicked");
+				
 			}//end for check multButton source
-		}
-	}//end class RadioListener
+		}//end actionPerformed method
+	}//end class SubmitListener
+
+
+
 }
 
 
