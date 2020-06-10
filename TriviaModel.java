e0d7410605a9bcd50f490a45e5be4a47bdeddd54
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-06-10 12:21:23 -0700

    Add files via upload

---
+++ TriviaModel.java
@@ -0,0 +1,19 @@
+package GUI;
+
+
+public class TriviaModel {
+
+	private int submitValue;
+	
+	public void setSubmitValue(int aNumber) {
+		
+		submitValue = aNumber;
+		
+	}
+	
+	public int getSubmitValue() {
+		return submitValue;
+	}
+
+	
+}
